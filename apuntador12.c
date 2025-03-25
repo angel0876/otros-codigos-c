804e8727e0d52c45fc31d11c8ef8706a76aa1b26
angel0876 <angelsanchezmartinez40@gmail.com>
2025-03-25 13:22:01 -0600

    codigosc

--- apuntador12.c
+++ apuntador12.c
@@ -7,5 +7,5 @@ int main (void)
     printf("\t\tentero\t\tfloatante\n\n");
     for(x=0; x<10; x++);
     printf("elemento %d:|t%d\t\t%d\n", x, &i[x],&f[x]);
-    //tarea medir los ciclos de reloj con apuntadores y sin apuntadores
+
 }
